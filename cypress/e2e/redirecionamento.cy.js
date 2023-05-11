5922609c543bc3a15757cde01e38ff87af216e31
yohanlimapo <yohan.limapo@gmail.com>
2023-05-11 19:02:45 -0300

    pasta it redirecionamento de paginas

---
+++ cypress/e2e/redirecionamento.cy.js
@@ -0,0 +1,23 @@
+describe('Redirecionamento a partir da página principal', ()=>{
+    beforeEach(() => {
+        cy.visit("http://web.kstack.com.br/v2/Home/Index")
+    })
+    
+    it('Página Kstacker', ()=> {
+        cy.contains('a', 'KSTACKER').click();
+        cy.url()
+        cy.location('span')
+        //Não estou sabendo fazer este it(), talvez porque a imagem está sobreposta.
+    })
+
+    it('Página Contato', ()=> {
+        cy.contains('a', 'CONTATO').click();
+        cy.contains('Nos conte como podemos te ajudar').should('be.visible')
+    })
+
+    it('Página Carreiras (vem ser KSK)', ()=> {
+        cy.contains('a', 'VEM PRA KSK').click();
+        cy.contains('Encontre a vaga que é sua cara').should('be.visible')
+    })
+
+})
