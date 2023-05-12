7865566efeb3c5bcf095cb673759cd38e0fd4516
yohanlimapo <yohan.limapo@gmail.com>
2023-05-12 08:34:39 -0300

    testes na nuvem

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'fcmwoe',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
